8dafb3278c6f25bead29067e8520696aff0fa78e
Felipe Rafael Rocha <109861866+FlpRocha236@users.noreply.github.com>
2026-03-16 16:42:06 -0300

    Update apiUrl to new endpoint

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -32,7 +32,7 @@ export interface StatusResponse {
 export class AudioService {
   // Ajuste: Removida a barra daqui para evitar duplicidade, 
   // mas garantindo que o caminho final SEMPRE tenha a barra.
-  private apiUrl = 'https://loud-feet-beg.loca.lt';
+  private apiUrl = 'https://0504428ce0edb9.lhr.life';
 
   constructor(private http: HttpClient) {}
 
